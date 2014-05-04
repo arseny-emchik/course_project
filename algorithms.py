ae5934f7a19f147293598c8a51d139493be1a931
ars <arsemchik@gmail.com>
2014-05-04 21:27:34 +0300

    refactor algorithms

--- algorithms.py
+++ algorithms.py
@@ -21,35 +21,66 @@ from random import randint
 # =======================================================
 
 # =======================================================
-#           Neural Network Interface
+#          Classifier Interface
 # =======================================================
-
-class InterfaceNN:
+class InterfaceClassifier:
     __metaclass__ = ABCMeta
 
-    # Do we really need this method when we have Control class?
+    # private class variables
+    __hFile = None
+    __csv_file = None
+    __row_count = None
+    __binary = False
+    __data_set = None
 
-    # @abstractmethod
-    # def showPlot(self):
-    #     """Show plot"""
+    # private class methods
+    def __get_full_data_set(self, num_inputs=-1, num_outputs=1):
+        num_inputs = self._count_inputs() if num_inputs == -1 else num_inputs
+        if num_inputs <= 0 or num_outputs <= 0:
+            return
 
-    @abstractmethod
-    def train(self,
-              cycles,
-              percent,
-              hidden_layers=3,
-              num_outputs=1,
-              num_inputs=-1,
-              hiddenclass=None):
-        """train net"""
+        self.__data_set = SupervisedDataSet(num_inputs, num_outputs)
+        self.hFile.seek(0)
+        for row in self.csv_file:
+            indata = row[:num_inputs]
+            outdata = row[num_inputs:]
+            self.__data_set.addSample(indata, outdata)
+        return self.__data_set
 
+    def __get_levels(self):
+        levels = []
+        for i in self.__data_set['target']:
+            level = int(i)
+            if level not in levels:
+                levels.append(level)
+        return levels
+
+    def __get_data_set_array_with_specific_level(self, level):
+        result = []
+        for i in self.__data_set:
+            current_level = int(i[1])
+            if current_level == level:
+                result.append(i)
+        return result
+
+    # protected class methods
+    def _count_inputs(self, number_inputs=-1):
+        self.hFile.seek(0)
+        for row in self.csv_file:
+            return len(row) - 1 if number_inputs == -1 else number_inputs
+        return 0
+
+    def _buildNet(self, hidden_layers, num_outputs, num_inputs, hiddenclass):
+        return buildNetwork(num_inputs, hidden_layers, num_outputs)
+
+    # public class methods
     def load_CSV(self, file_name):
         self.hFile = open(file_name, 'rb')
         self.csv_file = csv.reader(self.hFile)
         self.row_count = sum(1 for row in self.csv_file)
+        self.__data_set = self.__get_full_data_set()
 
-        self.binary = False
-        if self.get_levels() == 2:
+        if len(self.__get_levels()) == 2:
             self.binary = True
 
         return self.csv_file
@@ -60,49 +91,28 @@ class InterfaceNN:
         for row in self.csv_file:
             print row
 
-    def count_inputs(self, number_inputs=-1):
-        self.hFile.seek(0)
-        for row in self.csv_file:
-            return len(row) - 1 if number_inputs == -1 else number_inputs
-        return 0
-
-    def get_levels(self):
-        data_set = self.get_data_set(100)
-        levels = []
-        for i in data_set['target']:
-            level = int(i)
-            if level not in levels:
-                levels.append(level)
-        return levels
-
-    def get_data_set_array_with_specific_level(self, level):
-        data_set = self.get_data_set(100)
-        result = []
-        for i in data_set:
-            current_level = int(i[1])
-            if current_level == level:
-                result.append(i)
-        return result
-
     def is_binary(self):
-        return self.binary
+        return self.__binary
 
-    def get_data_set_by_levels(self, percent, num_inputs=-1, num_outputs=1):
-        levels = self.get_levels()
-        num_inputs = self.count_inputs() if num_inputs == -1 else num_inputs
-        if num_inputs <= 0 or num_outputs <= 0 or percent <= 0:
+    def get_data_set(self, percent, num_inputs=-1, num_outputs=1):
+        num_inputs = self._count_inputs() if num_inputs == -1 else num_inputs
+        if num_inputs <= 0 or num_outputs <= 0 or percent <= 0 or percent > 100:
             return
 
+        if percent == 100:
+            return self.__data_set
+
+        levels = self.__get_levels()
         ds = SupervisedDataSet(num_inputs, num_outputs)
         num_rows = int(self.row_count * (float(percent) / 100.0))
-        num_rows_in_class = int(num_rows/len(levels))
+        num_rows_in_class = int(num_rows / len(levels))
 
         for level in levels:
-            data_set = self.get_data_set_array_with_specific_level(level)
+            data_set = self.__get_data_set_array_with_specific_level(level)
             used_samples = []
             for i in range(num_rows_in_class):
                 added = False
-                while(not added):
+                while (not added):
                     row = randint(0, num_rows_in_class)
                     if row not in used_samples:
                         used_samples.append(row)
@@ -111,19 +121,23 @@ class InterfaceNN:
                         added = True
         return ds
 
-    def get_data_set(self, percent, num_inputs=-1, num_outputs=1):
-        num_inputs = self.count_inputs() if num_inputs == -1 else num_inputs
-        if num_inputs <= 0 or num_outputs <= 0 or percent <= 0:
-            return
-
-        ds = SupervisedDataSet(num_inputs, num_outputs)
-        self.hFile.seek(0)
-        for i in range(int(self.row_count * (float(percent) / 100.0))):
-            data = self.csv_file.next()
-            indata = data[:num_inputs]
-            outdata = data[num_inputs:]
-            ds.addSample(indata, outdata)
-        return ds
+    # abstract methods
+    @abstractmethod
+    def train(self,
+              cycles,
+              percent,
+              hidden_layers=3,
+              num_outputs=1,
+              num_inputs=-1,
+              hiddenclass=None):
+        """train net
+        :param cycles:
+        :param percent:
+        :param hidden_layers:
+        :param num_outputs:
+        :param num_inputs:
+        :param hiddenclass:
+        """
 
 # =======================================================
 #           Algorithms
@@ -132,11 +146,7 @@ class InterfaceNN:
 # =======================================================
 #           Back propagation
 # =======================================================
-
-class Backprop(InterfaceNN):
-    def buildNet(self, hidden_layers, num_outputs, num_inputs, hiddenclass):
-        return buildNetwork(num_inputs, hidden_layers, num_outputs)
-
+class Backprop(InterfaceClassifier):
     def train(self,
               cycles,
               percent,
@@ -144,11 +154,11 @@ class Backprop(InterfaceNN):
               num_outputs=1,
               num_inputs=-1,
               hiddenclass=None):
-        num_inputs = self.count_inputs() if num_inputs == -1 else num_inputs
+        num_inputs = self._count_inputs() if num_inputs == -1 else num_inputs
         if num_inputs <= 0 or num_outputs <= 0 or cycles <= 0 or (percent > 100 or percent <= 0):
             return
 
-        network = self.buildNet(hidden_layers, num_outputs, num_inputs, hiddenclass)
+        network = self._buildNet(hidden_layers, num_outputs, num_inputs, hiddenclass)
         data_set = self.get_data_set(percent, num_inputs, num_outputs)
         trainer = BackpropTrainer(network, data_set)
 
@@ -160,8 +170,7 @@ class Backprop(InterfaceNN):
 # =======================================================
 #           Resilient propagation
 # =======================================================
-
-class Rprop(Backprop):
+class Rprop(InterfaceClassifier):
     def train(self,
               cycles,
               percent,
@@ -169,11 +178,11 @@ class Rprop(Backprop):
               num_outputs=1,
               num_inputs=-1,
               hiddenclass=None):
-        num_inputs = self.count_inputs() if num_inputs == -1 else num_inputs
+        num_inputs = self._count_inputs() if num_inputs == -1 else num_inputs
         if num_inputs <= 0 or num_outputs <= 0 or cycles <= 0 or (percent > 100 or percent <= 0):
             return
 
-        network = self.buildNet(hidden_layers, num_outputs, num_inputs, hiddenclass)
+        network = self._buildNet(hidden_layers, num_outputs, num_inputs, hiddenclass)
         data_set = self.get_data_set(percent, num_inputs, num_outputs)
 
         trainer = RPropMinusTrainer(network, dataset=data_set)
@@ -186,14 +195,13 @@ class Rprop(Backprop):
 # =======================================================
 #           Decision Tree
 # =======================================================
-
-class DTree(InterfaceNN):
+class DTree(InterfaceClassifier):
     def train(self,
               percent,
               num_outputs=1,
               num_inputs=-1,
               hiddenclass=None):
-        num_inputs = self.count_inputs() if num_inputs == -1 else num_inputs
+        num_inputs = self._count_inputs() if num_inputs == -1 else num_inputs
         data_set = self.get_data_set(percent, num_inputs, num_outputs)
         clf = DecisionTreeClassifier().fit(data_set['input'], data_set['target'])
 
@@ -202,16 +210,14 @@ class DTree(InterfaceNN):
 # =======================================================
 #           Support Vector Machines
 # =======================================================
-
-class SVM(InterfaceNN):
+class SVM(InterfaceClassifier):
     def train(self,
               percent,
               num_outputs=1,
               num_inputs=-1,
               hiddenclass=None):
-        num_inputs = self.count_inputs() if num_inputs == -1 else num_inputs
+        num_inputs = self._count_inputs() if num_inputs == -1 else num_inputs
         data_set = self.get_data_set(percent, num_inputs, num_outputs)
-        clf = svm.SVC()
         clf = svm.SVC().fit(data_set['input'], np.ravel(data_set['target']))
 
         return clf
@@ -220,7 +226,8 @@ class SVM(InterfaceNN):
 #           CONTROL
 # =======================================================
 class Control:
-    def calculate_entire_ds(self, network, data_set):
+    # private class methods
+    def __calculate_entire_ds(self, network, data_set):
         y_true = []
         y_predict = []
 
@@ -235,8 +242,9 @@ class Control:
         # print y_predict
         return y_true, y_predict
 
+    # public methods
     def draw_confusion_matrix(self, network, data_set):
-        y_true, y_predict = self.calculate_entire_ds(network, data_set)
+        y_true, y_predict = self.__calculate_entire_ds(network, data_set)
         cm = confusion_matrix(y_true, y_predict)
         pl.matshow(cm)
         pl.title('Confusion matrix')
@@ -246,7 +254,7 @@ class Control:
         pl.show()
 
     def draw_roc(self, network, data_set):
-        y_true, y_predict = self.calculate_entire_ds(network, data_set)
+        y_true, y_predict = self.__calculate_entire_ds(network, data_set)
         fpr, tpr, thresholds = roc_curve(y_true, y_predict)
         roc_auc = auc(fpr, tpr)
         print("Area under the ROC curve : %f" % roc_auc)
@@ -264,24 +272,34 @@ class Control:
         pl.show()
 
 
+# tested 04.05.14 by ars
+c = Control()
 
-# #test
-# c = Control()
-# b = Backprop()
-# #b = Rprop()
-#
-# b.load_CSV('data_sets/new_iris_dataset.csv')
-# network = b.train(60, 90)
-# data_set = b.get_data_set(100)
+#test 1
+b = Backprop()
+b.load_CSV('data_sets/new_iris_dataset.csv')
+network = b.train(60, 90)
+data_set = b.get_data_set(100)
+
+c.draw_confusion_matrix(network, data_set)
+
+#test 2
+d = DTree()
+d.load_CSV('data_sets/new_iris_dataset.csv')
+network = d.train(90)
+data_set = d.get_data_set(100)
 
 #c.draw_confusion_matrix(network, data_set)
-# print "Predict y:\n{}".format(c.y_predict)
 
-#print b.get_data_set_by_levels(50)
+# test 3
+r = Rprop()
+d.load_CSV('data_sets/new_iris_dataset.csv')
+network = d.train(60, 90)
+data_set = d.get_data_set(100)
 
-# if b.is_binary():
-#     c.draw_roc(network, data_set)
+#c.draw_confusion_matrix(network, data_set)
 
+#test 4
 d = SVM()
 d.load_CSV('data_sets/new_iris_dataset.csv')
 clf = d.train(90)
@@ -289,3 +307,6 @@ data_set = d.get_data_set(100)
 
 for i in data_set:
    print clf.predict(i[0])
+
+# print "Predict y:\n{}".format(c.y_predict) ??!
+
